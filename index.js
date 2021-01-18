4ae64778f56b86f867f7eb0e209f840f39ff04c1
Gogi <gogion@protonmail.com>
2021-01-18 18:54:08 +0200

    removed 2nd button + unused classes in CSS +  created 2 functions for a single button in JS + switched from absolute to relative paths for the images in the index.html

--- index.js
+++ index.js
@@ -3,31 +3,30 @@ const bgColorClass = document.querySelector('.bg-color')
 const tag = document.querySelector('.tag')
 const lightModeBtn = document.getElementById('light-mode-btn')
 
-// Dark Mode Button
 darkModeBtn.addEventListener('click', () => {
+    if (document.body.style.backgroundColor === 'black') {
+    lightMode()
+  } else { 
+     darkMode()
+  }
+})
+
+
+// functions
+function darkMode() {
   document.body.style.backgroundColor = 'black'
   document.body.style.color = "white"
   bgColorClass.style.color = 'black'
 
   tag.style.backgroundColor = 'white'
   tag.style.color = 'black'
+}
 
-  darkModeBtn.style.display = 'none'
-  lightModeBtn.style.display = 'unset'
-
-})
-
-// Light Mode Button
-lightModeBtn.addEventListener('click', () => {
-  document.body.style.backgroundColor = 'initial'
+function lightMode () {
+  document.body.style.backgroundColor = 'white'
   document.body.style.color = "initial"
   bgColorClass.style.color = 'initial'
 
   tag.style.backgroundColor = 'black'
   tag.style.color = 'white'
-
-  darkModeBtn.style.display = 'initial'
-  lightModeBtn.style.display = 'none'
-})
-
-
+}
