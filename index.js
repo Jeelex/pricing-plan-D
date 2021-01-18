08359cd3107f2c6b0beb5b447bfde4b12c0f2e61
Gogi <gogion@protonmail.com>
2021-01-18 18:56:34 +0200

    added h1 for better  accessibility

--- index.js
+++ index.js
@@ -4,27 +4,26 @@ const tag = document.querySelector('.tag')
 const lightModeBtn = document.getElementById('light-mode-btn')
 
 darkModeBtn.addEventListener('click', () => {
-    if (document.body.style.backgroundColor === 'black') {
+  if (document.body.style.backgroundColor === 'black') {
     lightMode()
-  } else { 
-     darkMode()
+  } else {
+    darkMode()
   }
 })
-
 
 // functions
 function darkMode() {
   document.body.style.backgroundColor = 'black'
-  document.body.style.color = "white"
+  document.body.style.color = 'white'
   bgColorClass.style.color = 'black'
 
   tag.style.backgroundColor = 'white'
   tag.style.color = 'black'
 }
 
-function lightMode () {
+function lightMode() {
   document.body.style.backgroundColor = 'white'
-  document.body.style.color = "initial"
+  document.body.style.color = 'initial'
   bgColorClass.style.color = 'initial'
 
   tag.style.backgroundColor = 'black'
